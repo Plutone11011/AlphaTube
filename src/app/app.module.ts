8271f5f8d6dbb3dafad3da28d072bcec23693724
Plutone11011 <lorenzo97@fastwebnet.it>
2018-06-16 23:16:09 +0200

    un primo router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RecommenderComponent } from './recommender/recommender.component';
+import { ContentVideoComponent } from './content-video/content-video.component';
+import { InfoComponent } from './info/info.component';
+import { AppRoutingModule } from './/app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecommenderComponent,
+    ContentVideoComponent,
+    InfoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
