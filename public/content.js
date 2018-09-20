3b039af4764ed8758299706de7640006c1991f03
Plutone11011 <lorenzo97@fastwebnet.it>
2018-09-20 16:39:22 +0200

    changed directory for view

--- public/content.js
+++ public/content.js
@@ -48,6 +48,8 @@ function setContentBrano(videoId){
                         "PREFIX dbo: <http://dbpedia.org/ontology/>", 
                         "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>",
                         "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>",
+                        "PREFIX mo: <http://purl.org/ontology/mo/>", 
+                        "PREFIX dc: <http://purl.org/dc/elements/1.1/>",
                         //"PREFIX foaf: <http://xmlns.com/foaf/0.1/>",
                         //"PREFIX sg: <http://dbpedia.org/resource/song>",
                         "SELECT ?abstract ?music",
