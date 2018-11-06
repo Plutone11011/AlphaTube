ceb90d73b85c75e240d228aa651ebe9b8c5ee91e
Plutone11011 <lorenzo97@fastwebnet.it>
2018-11-06 19:46:05 +0100

    random recommender e iframe

--- public/search.js
+++ public/search.js
@@ -15,7 +15,9 @@ $(document).ready(function(){
 				videoCategoryId:'10'
 			}).done(function(data){
 				var counter ;
-				$('iframe').attr('src','https://www.youtube.com/embed/' + data.items[0].id.videoId) ;
+				//$('iframe').attr('src','https://www.youtube.com/embed/' + data.items[0].id.videoId+
+				//	'?enablejsapi=1&origin=http://localhost:3000') ; //questo fa perdere controllo api
+				player.loadVideoById(data.items[0].id.videoId,0,'large');
 				setDescription(data.items[0].snippet.description);
 				setComments(data.items[0].id.videoId);
 				setContentBrano(data.items[0].id.videoId);
@@ -23,11 +25,11 @@ $(document).ready(function(){
 				//create thumbnail list of the remaining 29 videos
 				//need to add logic to clean recommender area after search by id
 				$.each(data.items ,function(index, value){
-					$('.listevideo').append(thumbSearch);
+					$('#recommenderSearch').append(thumbSearch);
 					counter = index + 1;
 					$('a:nth-child(' + counter.toString() + ') > img').attr('src',value.snippet.thumbnails.medium.url); //vedi search e snippet
-					counter = parseInt(counter);
-				
+					//counter = parseInt(counter);
+					
 				});
 			});
 	}
@@ -48,7 +50,37 @@ $(document).ready(function(){
     			else{
     				searchVideos(query,30); //input is not a video id
     			}
-    		}) 
+    		});
+		console.log(window.history); 
 	});
+	
+
+
+	function randomDate(start, end) {
+    	return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+	}
+
+	var data1 = randomDate(new Date(2005, 4, 25), new Date());
+	var data2 = new Date(data1);
+	data2.setMonth(data1.getMonth()+1);
+	console.log(data1,data2);
+	$.get('https://www.googleapis.com/youtube/v3/search',{
+		part: 'snippet',
+		key:'AIzaSyADhaxguT2HLfSM0ALnEpejHVvZw1vlbFU',
+		publishedAfter: data1.toISOString(),
+		publishedBefore: data2.toISOString(),
+		videoCategoryId: '10',
+		type:'video',
+		maxResults: 30
+	}).done((data)=>{
+		console.log(data);
+		$.each(data.items ,function(index, value){
+			$('#recommenderRandom').append(thumbSearch);
+			counter = index + 1;
+			$('a:nth-child(' + counter.toString() + ') > img').attr('src',value.snippet.thumbnails.medium.url); //vedi search e snippet
+			//counter = parseInt(counter);
+			
+		});
+	})
 	
 });
